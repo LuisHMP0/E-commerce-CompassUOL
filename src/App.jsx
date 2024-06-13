810930fafb2294a6ef0957c577b7ebb258230a0a
LuisHMP0 <148148975+LuisHMP0@users.noreply.github.com>
2024-06-13 15:54:03 -0300

    style(produtos): changing margin styling for cleaner code
    i added a flex-wrap so that every 4 products creates a new product line, this way, it will not be
    necessary to create a div for each line.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import './App.css'
 import Header from './components/Header/Header'
 import Section01 from '/src/components/Section01/Section01.jsx'
--- src/components/Produtos/Produtos.jsx
+++ src/components/Produtos/Produtos.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import React from 'react'
 import './Produtos.css'
 import Produto from '../Produto/Produto'
@@ -6,139 +7,131 @@ import LeviosaIMG from './img/Leviosa.png'
 import LolitoIMG from './img/Lolito.png'
 import RespiraIMG from './img/Respira.png'
 import Botoes from '../Botoes/Botoes'
+import Section02 from '../Section02/Section02'
 
 
 const Produtos = () => {
-  return (
-    <>
-    <div className='produtos'>
-        <Produto
-            src={SytherineIMG}
-            titulo={'Syltherine'}
-            subtitulo={'Stylish cafe chair'}
-            preco={'2.500.000'}
-            precoRiscado={'Rp 3.500.000'}
-            desconto={'-30%'}
-        />
-        <Produto
-            src={LeviosaIMG}
-            titulo={'Leviosa'}
-            subtitulo={'Stylish cafe chair'}
-            preco={'2.500.000'}
-        />
-        <Produto 
-            src={LolitoIMG}
-            titulo={'Lolito'}
-            subtitulo={'Luxury big sofa'}
-            preco={'7.000.000'}
-            precoRiscado={'Rp 14.000.000'}
-            desconto={'-50%'}
-        />
-        <Produto 
-            src={RespiraIMG}
-            titulo={'Respira'}
-            subtitulo={'Outdoor bar table and stool'}
-            preco={'500.000'}
-            novoProduto={true}
-        />
-    </div>
-    <div className='produtos'>
-        <Produto
-            src={SytherineIMG}
-            titulo={'Syltherine'}
-            subtitulo={'Stylish cafe chair'}
-            preco={'2.500.000'}
-            precoRiscado={'Rp 3.500.000'}
-            desconto={'-30%'}
-        />
-        <Produto
-            src={LeviosaIMG}
-            titulo={'Leviosa'}
-            subtitulo={'Stylish cafe chair'}
-            preco={'2.500.000'}
-        />
-        <Produto 
-            src={LolitoIMG}
-            titulo={'Lolito'}
-            subtitulo={'Luxury big sofa'}
-            preco={'7.000.000'}
-            precoRiscado={'Rp 14.000.000'}
-            desconto={'-50%'}
-        />
-        <Produto 
-            src={RespiraIMG}
-            titulo={'Respira'}
-            subtitulo={'Outdoor bar table and stool'}
-            preco={'500.000'}
-            novoProduto={true}
-        />
-    </div>
-    <div className='produtos'>
-        <Produto
-            src={SytherineIMG}
-            titulo={'Syltherine'}
-            subtitulo={'Stylish cafe chair'}
-            preco={'2.500.000'}
-            precoRiscado={'Rp 3.500.000'}
-            desconto={'-30%'}
-        />
-        <Produto
-            src={LeviosaIMG}
-            titulo={'Leviosa'}
-            subtitulo={'Stylish cafe chair'}
-            preco={'2.500.000'}
-        />
-        <Produto 
-            src={LolitoIMG}
-            titulo={'Lolito'}
-            subtitulo={'Luxury big sofa'}
-            preco={'7.000.000'}
-            precoRiscado={'Rp 14.000.000'}
-            desconto={'-50%'}
-        />
-        <Produto 
-            src={RespiraIMG}
-            titulo={'Respira'}
-            subtitulo={'Outdoor bar table and stool'}
-            preco={'500.000'}
-            novoProduto={true}
-        />
-    </div>
-    <div className='produtos'>
-        <Produto
-            src={SytherineIMG}
-            titulo={'Syltherine'}
-            subtitulo={'Stylish cafe chair'}
-            preco={'2.500.000'}
-            precoRiscado={'Rp 3.500.000'}
-            desconto={'-30%'}
-        />
-        <Produto
-            src={LeviosaIMG}
-            titulo={'Leviosa'}
-            subtitulo={'Stylish cafe chair'}
-            preco={'2.500.000'}
-        />
-        <Produto 
-            src={LolitoIMG}
-            titulo={'Lolito'}
-            subtitulo={'Luxury big sofa'}
-            preco={'7.000.000'}
-            precoRiscado={'Rp 14.000.000'}
-            desconto={'-50%'}
-        />
-        <Produto 
-            src={RespiraIMG}
-            titulo={'Respira'}
-            subtitulo={'Outdoor bar table and stool'}
-            preco={'500.000'}
-            novoProduto={true}
-        />
-    </div>
-    <div className='produtos'>
-        <Botoes />
-    </div>
-    
+    return (
+        <>
+        <div className='produtos'>
+            <Produto
+                src={SytherineIMG}
+                titulo={'Syltherine'}
+                subtitulo={'Stylish cafe chair'}
+                preco={'2.500.000'}
+                precoRiscado={'Rp 3.500.000'}
+                desconto={'-30%'}
+            />
+            <Produto
+                src={LeviosaIMG}
+                titulo={'Leviosa'}
+                subtitulo={'Stylish cafe chair'}
+                preco={'2.500.000'}
+            />
+            <Produto 
+                src={LolitoIMG}
+                titulo={'Lolito'}
+                subtitulo={'Luxury big sofa'}
+                preco={'7.000.000'}
+                precoRiscado={'Rp 14.000.000'}
+                desconto={'-50%'}
+            />
+            <Produto 
+                src={RespiraIMG}
+                titulo={'Respira'}
+                subtitulo={'Outdoor bar table and stool'}
+                preco={'500.000'}
+                novoProduto={true}
+            />
+                        <Produto
+                src={SytherineIMG}
+                titulo={'Syltherine'}
+                subtitulo={'Stylish cafe chair'}
+                preco={'2.500.000'}
+                precoRiscado={'Rp 3.500.000'}
+                desconto={'-30%'}
+            />
+            <Produto
+                src={LeviosaIMG}
+                titulo={'Leviosa'}
+                subtitulo={'Stylish cafe chair'}
+                preco={'2.500.000'}
+            />
+            <Produto 
+                src={LolitoIMG}
+                titulo={'Lolito'}
+                subtitulo={'Luxury big sofa'}
+                preco={'7.000.000'}
+                precoRiscado={'Rp 14.000.000'}
+                desconto={'-50%'}
+            />
+            <Produto 
+                src={RespiraIMG}
+                titulo={'Respira'}
+                subtitulo={'Outdoor bar table and stool'}
+                preco={'500.000'}
+                novoProduto={true}
+            />
+                        <Produto
+                src={SytherineIMG}
+                titulo={'Syltherine'}
+                subtitulo={'Stylish cafe chair'}
+                preco={'2.500.000'}
+                precoRiscado={'Rp 3.500.000'}
+                desconto={'-30%'}
+            />
+            <Produto
+                src={LeviosaIMG}
+                titulo={'Leviosa'}
+                subtitulo={'Stylish cafe chair'}
+                preco={'2.500.000'}
+            />
+            <Produto 
+                src={LolitoIMG}
+                titulo={'Lolito'}
+                subtitulo={'Luxury big sofa'}
+                preco={'7.000.000'}
+                precoRiscado={'Rp 14.000.000'}
+                desconto={'-50%'}
+            />
+            <Produto 
+                src={RespiraIMG}
+                titulo={'Respira'}
+                subtitulo={'Outdoor bar table and stool'}
+                preco={'500.000'}
+                novoProduto={true}
+            />
+                        <Produto
+                src={SytherineIMG}
+                titulo={'Syltherine'}
+                subtitulo={'Stylish cafe chair'}
+                preco={'2.500.000'}
+                precoRiscado={'Rp 3.500.000'}
+                desconto={'-30%'}
+            />
+            <Produto
+                src={LeviosaIMG}
+                titulo={'Leviosa'}
+                subtitulo={'Stylish cafe chair'}
+                preco={'2.500.000'}
+            />
+            <Produto 
+                src={LolitoIMG}
+                titulo={'Lolito'}
+                subtitulo={'Luxury big sofa'}
+                preco={'7.000.000'}
+                precoRiscado={'Rp 14.000.000'}
+                desconto={'-50%'}
+            />
+            <Produto 
+                src={RespiraIMG}
+                titulo={'Respira'}
+                subtitulo={'Outdoor bar table and stool'}
+                preco={'500.000'}
+                novoProduto={true}
+            />
+            <Botoes />
+        </div>
     </>
 
     

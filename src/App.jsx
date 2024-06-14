719f99237ffa89278c25dc4d49752cfe356dbda8
LuisHMP0 <148148975+LuisHMP0@users.noreply.github.com>
2024-06-14 14:52:43 -0300

    style: adding responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css'
 import Header from './components/Header/Header'
 import Section01 from '/src/components/Section01/Section01.jsx'
-import Section02 from './components/Section02/Section02'
 import Produtos from './components/Produtos/Produtos'
 import Section03 from './components/Section03/Section03'
 import Footer from './components/Footer/Footer'
